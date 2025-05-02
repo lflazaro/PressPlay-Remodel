e1e1ffd32dac3aedc846558390c8f134305523ce
blank <atenmusicworkscontact@gmail.com>
2025-05-02 12:58:09 +0200

    12:58 02/05/2025

---
+++ PressPlay/Converters/FrameToPositionConverter.cs
@@ -0,0 +1,45 @@
+﻿// PressPlay/Converters/FrameToPositionConverter.cs
+using PressPlay.Helpers;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace PressPlay.Converters
+{
+    public class FrameToPositionConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int frame)
+            {
+                // Get zoom level from parameter or use default
+                int zoomLevel = 1;
+                if (parameter is int zoom)
+                    zoomLevel = zoom;
+
+                return frame * Constants.TimelinePixelsInSeparator / Constants.TimelineZooms[zoomLevel];
+            }
+            return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double position)
+            {
+                // Get zoom level from parameter or use default
+                int zoomLevel = 1;
+                if (parameter is int zoom)
+                    zoomLevel = zoom;
+
+                return (int)(position * Constants.TimelineZooms[zoomLevel] / Constants.TimelinePixelsInSeparator);
+            }
+            return 0;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
---
+++ PressPlay/Converters/FrameToWidthConverter.cs
@@ -0,0 +1,45 @@
+﻿// PressPlay/Converters/FrameToWidthConverter.cs
+using PressPlay.Helpers;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace PressPlay.Converters
+{
+    public class FrameToWidthConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int frameCount)
+            {
+                // Get zoom level from parameter or use default
+                int zoomLevel = 1;
+                if (parameter is int zoom)
+                    zoomLevel = zoom;
+
+                return frameCount * Constants.TimelinePixelsInSeparator / Constants.TimelineZooms[zoomLevel];
+            }
+            return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double width)
+            {
+                // Get zoom level from parameter or use default
+                int zoomLevel = 1;
+                if (parameter is int zoom)
+                    zoomLevel = zoom;
+
+                return (int)(width * Constants.TimelineZooms[zoomLevel] / Constants.TimelinePixelsInSeparator);
+            }
+            return 0;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
